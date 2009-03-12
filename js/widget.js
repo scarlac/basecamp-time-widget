79c1749f73db9f871ce36a350fdfb0fe6455261c
Seph Soliman <ss@konstellation.dk>
2009-03-12 11:33:10 +0100

    ability to fetch projects and companies from basecamp and insert into dummy drop down

--- js/widget.js
+++ js/widget.js
@@ -1,9 +1,23 @@
-var gDoneButton;
-var gInfoButton;
+var gDoneButton = null;
+var gInfoButton = null;
+var allProjects = null;
+var allCompanies = null;
+var ajaxOptions = {
+	type: 'GET',
+	username: '',
+	password: ''
+};
 
 function setup() {
 	$("#infoButton").text("i").click(showPrefs);
 	$("#doneButton").text("Tilbage").click(hidePrefs);
+	
+	// * set externally in localsettings.js
+	ajaxOptions.username = bc_username;
+	ajaxOptions.password = bc_password;
+	
+	allProjects = {};
+	allCompanies = {};
 }
 
 function showPrefs() {
@@ -31,52 +45,116 @@ function hidePrefs() {
 	back.style.display="none";
 	front.style.display="block";
 	
-	if (window.widget)
+	if(window.widget)
 		setTimeout('widget.performTransition();', 0);
 }
 
-var projects = {};
-var companies = {};
-function dotest() {
-	// * set externally in localsettings.js
-	var username=bc_username;
-	var password=bc_password;
-	var baseURL = bc_base_url;
-
-	var projectsURL = baseURL + "/projects.xml";
-	
-	$.ajaxSetup({
-		username: username,
-		password: password
-	});
-	
+function loadStuff() {
 	pullProjects(); // * Will also pull project todos, etc.
 }
 
 function pullProjects() {
-	$.ajax(feedURL, parseProjects);
+	var projectsURL = bc_base_url + "/projects.xml";
+	var opts = ajaxOptions;
+	
+	console.log('pulling projects data from ' + projectsURL);
+
+	opts.url = projectsURL;
+	opts.success = parseProjects;
+	$.ajax(opts);
 }
 
-function pullProjectTodos() {
-	$.ajax(feedURL, parseProjects);
+function pullProjectTodos(id) {
+	var todoURL = bc_base_url + "/projects/"+id+".xml";
+	var opts = ajaxOptions;
+	
+	console.log('pulling project todos from ' + todoURL);
+
+	opts.url = todoURL;
+	opts.success = parseProjectTodos;
+	$.ajax(opts);
 }
 
 function parseProjects(projectsNode) {
-	window.projectsNode = projectsNode;
-	$(projectsNode).find("projects project").each(function(i) {
-		var t = $(this);
-		var id = t.find("id").text();
-		projects[id] = {
-			id: id,
-			name: t.find("name").text(),
-			status: t.find("status").text()
-		}
-	});
-	pullProjectTodos();
-	alert('got response from server: ' + $.param(projects));
-}
-
-function parseProject(projectNode) {
+	console.log('parsing projects data');
 	
+	allProjects = {};
+	allCompanies = {};
+	$(projectsNode).find("projects > project").each(function(i) {
+		var t = $(this);
+		
+		var id = t.find("> id").text();
+		var name = t.find("> name").text();
+		var prj = new Project(id, name);
+		
+		var c_id = t.find("> company > id").text();
+		var c_name = t.find("> company > name").text();
+		var cmp = new Company(c_id, c_name);
+		
+		prj.company = cmp;
+		
+		allProjects[prj.id] = prj;
+		allCompanies[cmp.id] = cmp;
+	});
+	
+	// * setup the drop down
+	console.log('setting up projects drop down');
+	$("#projects").html('<option value="">- Select project -</option>');
+	for(var i in allCompanies) {
+		var cmp = allCompanies[i];
+		var projects = cmp.getProjects();
+		$("#projects").append('<option value="" disabled="disabled">'+cmp.name+'</option>');
+		for(var i in projects) {
+			var prj = projects[i];
+			$("#projects").append('<option value="'+prj.id+'">&nbsp;&nbsp;'+prj.name+'</option>');
+		}
+	}
+	
+	pullProjectTodos();
 }
 
+function parseProjectTodos(rootNode) {
+	console.log('parsing project todos');
+}
+
+Project = function(id, name) {
+	this.id = id;
+	this.name = name;
+}
+
+Company = function(id, name) {
+	this.id = id;
+	this.name = name;
+}
+
+Company.prototype.getProjects = function() {
+	var projects = {};
+	
+	for(var i in allProjects) {
+		var prj = allProjects[i];
+		if(prj.company.id == this.id) {
+			projects[prj.id] = prj;
+		}
+	}
+	
+	return projects;
+}
+
+/*
+<projects type="array">
+  <project>
+    <announcement nil="true"></announcement>
+    <created-on type="date">2009-01-05</created-on>
+    <id type="integer">42</id>
+    <last-changed-on type="datetime">2009-03-09T09:24:34Z</last-changed-on>
+    <name>My project name</name>
+    <show-announcement type="boolean">false</show-announcement>
+    <show-writeboards type="boolean">true</show-writeboards>
+    <start-page>log</start-page>
+    <status>active</status>
+    <company>
+      <id type="integer">1234567</id>
+      <name>Simcorp</name>
+    </company>
+  </project>
+*/
